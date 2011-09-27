c4302fb2b445f126ed45dbb663cb6205cc514945
Mikko Haapalehto <mikko.haapalehto@gmail.com>
2011-09-27 11:28:29 +0300

    adv service

---
+++ AdGetAndroidWidget/src/adget/android/service/AdvertisementService.java
@@ -0,0 +1,15 @@
+package adget.android.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AdvertisementService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
