51a5665239b6adefea29c30f64c45342b7bca314
Mikko Haapalehto <mikko.haapalehto@gmail.com>
2011-09-27 11:02:23 +0300

    duuni commmit

--- AdGetAndroidWidget/src/adget/android/servicehelper/LocationService.java
+++ AdGetAndroidWidget/src/adget/android/servicehelper/LocationService.java
@@ -1,12 +1,8 @@
 package adget.android.servicehelper;
 
-import java.util.List;
-
-import adget.android.servicehelper.to.Advertisement;
 import adget.android.servicehelper.to.Location;
 
 public interface LocationService {
 
 	public Location getLocation();
-	public List<Advertisement> getSurroundingAdvertisements(Location location);
 }
